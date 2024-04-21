45f51ff0c3da060520c3c8ab2e947d9399eabf9c
haroldchen <45569004+george-harold@users.noreply.github.com>
2024-04-21 21:12:56 +0800

    Add files via upload

---
+++ long_image_splicing/long_image_splicing.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/env python
+# author: harold-chen
+# website: https://github.com/george-harold/some_tools
+
+from PIL import Image
+import numpy as np
+import glob
+import os
+
+import numpy as np
+from PIL import Image
+
+def concatenate_images(image_paths, vertical=True):
+    imageArrays = [np.array(Image.open(path)) for path in image_paths]
+    axis = 0 if vertical else 1
+    resultImage = Image.fromarray(np.concatenate(imageArrays, axis=axis))
+
+    return resultImage
+
+def save_concatenated_images(image_paths, batch_size=4, vertical=True, save_path="image_{}.jpg"):
+    for i in range(0, len(image_paths), batch_size):
+        batch_paths = image_paths[i :i+batch_size]
+        result_image = concatenate_images(batch_paths, vertical)
+
+        result_image.save(save_path.format(i // batch_size + 1))
+
+imagePaths = list(glob.glob(os.path.join("dataset", "*")))
+save_concatenated_images(imagePaths, batch_size=4, vertical=True)
